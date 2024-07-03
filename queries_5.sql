14eaf315d14f54080d80d23ebcc6ad7baabd3964
Anastasia <nastyadelonge_1@mail.ru>
2024-07-04 00:05:22 +0300

    Add files via upload

---
+++ queries_5.sql
@@ -0,0 +1,43 @@
+--Написать запрос, который:
+--1. Выведет актуальное состояние товаров на 2020-06-01
+--2. Товары, купленные по цене больше или равно чем 3
+--3. Сумму покупок клиента 1
+--4. Сумма всех покупок до 2020-05-01 включительно
+--5. Сумму всех заказов и среднее цена заказа поквартально
+
+
+--1
+select i.name, o.order_date
+from items2 i
+left join orders3 o on i.item_id = o.item_id
+where o.order_date = '2020-06-01'
+
+--2
+select i.name, i.price
+from items2 i
+left join orders3 o on i.item_id = o.item_id
+where i.price >= 3
+
+--3
+select sum(i.price)
+from items2 i
+left join orders3 o on i.item_id = o.item_id
+where o.user_id = 1
+
+--4 
+select sum(i.price)
+from items2 i
+left join orders3 o on i.item_id = o.item_id
+where o.order_date <= '2020-05-01'
+
+
+--5
+select 
+	   date_trunc('quarter', o.order_date) as quarter,
+	   sum(i.price) as total_spent,
+       avg(i.price) as avg_order_price
+from items2 i
+left join orders3 o on i.item_id = o.item_id
+group by quarter 
+order by quarter  
+
