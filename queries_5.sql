5ee79afcda91d1191f5928c92bb649e77abff8e4
Anastasia <nastyadelonge_1@mail.ru>
2024-07-17 12:25:40 +0300

    Update queries_5.sql

--- queries_5.sql
+++ queries_5.sql
@@ -7,37 +7,57 @@
 
 
 --1
-select i.name, o.order_date
-from orders3 o
-left join items2 i on i.item_id = o.item_id
-where o.order_date = '2020-06-01'
+select 
+	i.name, o.order_date
+from 
+	orders3 o
+left join 
+	items2 i on i.item_id = o.item_id and i.update_date = o.order_date
+where 
+	o.order_date = '2020-06-01'
 
 --2
-select i.name, i.price
-from orders3 o
-left join items2 i on i.item_id = o.item_id  !! and date! 
-where i.price >= 3
+select 
+	i.name, i.price
+from 
+	orders3 o
+left join 
+	items2 i on i.item_id = o.item_id and i.update_date = o.order_date 
+where 
+	i.price >= 3
 
 --3
-select sum(i.price)
-from orders3 o
-left join items2 i on i.item_id = o.item_id   !!date! 
-where o.user_id = 1
+select 
+	sum(i.price)
+from 
+	orders3 o
+left join 
+	items2 i on i.item_id = o.item_id and i.update_date = o.order_date
+where 
+	o.user_id = 1
 
 --4 
-select sum(i.price)
-from orders3 o
-left join items2 i on i.item_id = o.item_id   !!date! 
-where o.order_date <= '2020-05-01'
+select 
+	sum(i.price)
+from 
+	orders3 o
+left join 
+	items2 i on i.item_id = o.item_id and i.update_date = o.order_date
+where 
+	o.order_date <= '2020-05-01'
 
 
 --5
 select 
-	   date_trunc('quarter', o.order_date) as quarter,
-	   sum(i.price) as total_spent,
-       avg(i.price) as avg_order_price
-from orders3 o
-left join items2 i on i.item_id = o.item_id   !!!date! 
-group by quarter 
-order by quarter  
+	date_trunc('quarter', o.order_date) as quarter,
+	sum(i.price) as total_spent,
+	avg(i.price) as avg_order_price
+from 
+	orders3 o
+left join 
+	items2 i on i.item_id = o.item_id and i.update_date = o.order_date
+group by 
+	quarter 
+order by 
+	quarter  
 
